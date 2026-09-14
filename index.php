c95fc882fa2be7e4c56026286471133f6ee262b1
cliffamadeus <dev.clfm5t12@gmail.com>
2026-09-14 15:36:41 +0800

    Added display table for activity logs on admin/index.php

--- index.php
+++ index.php
@@ -18,13 +18,25 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if ($login==='' || $password ===''){
         
         // Log incomplete login attempt
-       logActivity($pdo,null,$login,'login','failed');
+       logActivity(
+            $pdo,
+            null,
+            $login,
+            'login',
+            'failed'
+        );
 
     } else {
 
         if(loginUser($pdo,$login,$password)){
             // Log complete login attempt
-            logActivity($pdo,$_SESSION['user_id'],$_SESSION['user_email'],'login','success');
+
+            logActivity(
+                $pdo,$_SESSION['user_id'],
+                $_SESSION['user_email'],
+                'login',
+                'success'
+            );
 
             echo 'Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php';
             header('Location: ' . BASE_URL . '/app/' . $_SESSION['user_role'] . '/index.php');
